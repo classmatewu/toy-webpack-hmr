0e525a2f0840c27e406fc08218af32e2f79e158c
zelinwu <zelinwu@tencent.com>
2021-10-20 14:50:51 +0800

    test(server): :alembic: use webpack-dev-middleware replace webpack watch api 1. 发现webpack-dev-middleware内部已经集成了内存文件系统、watch等API 2. webpack-dev-middleware是webpack-dev-server的非常重要的插件 3. 我们自己实现一个 webpack-dev-middleware

--- server/app.js
+++ server/app.js
@@ -1,24 +1,20 @@
 const express = require('express')
 const webpack = require('webpack')
 const config = require('../webpack.config')
+const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = new express()
 const compiler = webpack(config)
 
-let changeNum = 0
-compiler.watch({/** watchOptions **/}, () => {
-  console.log('change ... ');
-  changeNum++
-})
 
-app.use((req, res, next) => {
-  console.log('hhh');
-  res.send({
-    code: 0,
-    msg: 'success',
-    data: changeNum
+// 告知 express 使用 webpack-dev-middleware，
+// 以及将 webpack.config.js 配置文件作为基础配置。
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
   })
-})
+);
+
 
 app.listen('3322', () => {
   console.log('127.0.0.1:3322');
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
      */
     output : {
         filename : 'bundle.[hash:8].js',
-        path : path.join(__dirname, './dist')
+        path : path.join(__dirname, './dist'),
+        publicPath: '/',
     },
 }
